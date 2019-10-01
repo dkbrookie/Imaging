701627802b23f807888899d7d4616e73d0a01c2b
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-10-01 14:06:22 -0500

    Update Windows_10_1903_Clean_Install.ps1
    -Fixed some annoying spacing issues, no actual edits made to the code

--- Windows_10_Staging/Powershell/Windows_10_1903_Clean_Install.ps1
+++ Windows_10_Staging/Powershell/Windows_10_1903_Clean_Install.ps1
@@ -1,24 +1,24 @@
 #region functions
 Function Get-Tree($Path,$Include='*') {
-  @(Get-Item $Path -Include $Include -Force) +
+    @(Get-Item $Path -Include $Include -Force) +
     (Get-ChildItem $Path -Recurse -Include $Include -Force) | Sort PSPath -Descending -Unique
 }
 
 Function Remove-Tree($Path,$Include='*') {
-  Get-Tree $Path $Include | Remove-Item -Force -Recurse
+    Get-Tree $Path $Include | Remove-Item -Force -Recurse
 }
 
 Function Install-withProgress {
-  $localFolder= (Get-Location).path
-  $process = Start-Process -FilePath "$localFolder\Installer.exe" -ArgumentList "/silent /accepteula" -PassThru
-  For($i = 0; $i -le 100; $i = ($i + 1) % 100) {
-    Write-Progress -Activity "Installer" -PercentComplete $i -Status "Installing"
-    Start-Sleep -Milliseconds 100
-    If ($process.HasExited) {
-        Write-Progress -Activity "Installer" -Completed
-        Break
+    $localFolder= (Get-Location).path
+    $process = Start-Process -FilePath "$localFolder\Installer.exe" -ArgumentList "/silent /accepteula" -PassThru
+    For($i = 0; $i -le 100; $i = ($i + 1) % 100) {
+        Write-Progress -Activity "Installer" -PercentComplete $i -Status "Installing"
+        Start-Sleep -Milliseconds 100
+        If ($process.HasExited) {
+            Write-Progress -Activity "Installer" -Completed
+            Break
+        }
     }
-  }
 }
 #endregion functions
 
@@ -26,9 +26,9 @@ Function Install-withProgress {
 ## we want to make sure to put it back in that location after the win10 image is installed
 $locationID = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\LabTech\Service" -Name LocationID
 If (!$locationID) {
-  Write-Warning 'No LocationID found for this machine, Automate agent not installed'
+    Write-Warning 'No LocationID found for this machine, Automate agent not installed'
 } Else {
-  Write-Output "Automate LocationdID is $locationID"
+    Write-Output "Automate LocationdID is $locationID"
 }
 
 
@@ -38,8 +38,8 @@ If (!$locationID) {
 ## 10 is more than we need but just playing it safe.
 $spaceAvailable = [math]::round((Get-PSDrive C | Select -ExpandProperty Free) / 1GB,0)
 If ($spaceAvailable -lt 10) {
-  Write-Warning "You only have a total of $spaceAvailable GBs available, this upgrade needs 10GBs or more to complete successfully"
-  Break
+    Write-Warning "You only have a total of $spaceAvailable GBs available, this upgrade needs 10GBs or more to complete successfully"
+    Break
 }
 #region checkDisk
 
@@ -59,18 +59,18 @@ If ($rbCheck1 -ne $Null -or $rbCheck2 -ne $Null -or $rbCheck3 -ne $Null){
 
 ## Determine if the machine is x86 or x64
 Try {
-  If ((Get-WmiObject win32_operatingsystem | Select-Object -ExpandProperty osarchitecture) -eq '64-bit') {
-    ## This is the size of the 64-bit file once downloaded so we can compare later and make sure it's complete
-    $servFile = 4827807744
-    $osArch = 'x64'
-  } Else {
-    ## This is the size of the 32-bit file once downloaded so we can compare later and make sure it's complete
-    $servFile = 3266728252
-    $osArch = 'x86'
-  }
+    If ((Get-WmiObject win32_operatingsystem | Select-Object -ExpandProperty osarchitecture) -eq '64-bit') {
+        ## This is the size of the 64-bit file once downloaded so we can compare later and make sure it's complete
+        $servFile = 4827807744
+        $osArch = 'x64'
+    } Else {
+        ## This is the size of the 32-bit file once downloaded so we can compare later and make sure it's complete
+        $servFile = 3266728252
+        $osArch = 'x86'
+    }
 } Catch {
-  Write-Warning 'Unable to determine OS architecture'
-  Return
+    Write-Warning 'Unable to determine OS architecture'
+    Return
 }
 #endregion checkOSInfo
 
@@ -86,7 +86,7 @@ $SetupComplete = "$1903Dir\SetupComplete.cmd"
 $SetupCompleteContent = "https://raw.githubusercontent.com/dkbrookie/Imaging/master/Windows_10_Staging/Bat/SetupComplete.cmd"
 ## Check for the main directory we're going to work from, create it if it doesn't exist
 If (!(Test-Path $1903Dir)) {
-  New-Item -Path $1903Dir -ItemType Directory | Out-Null
+    New-Item -Path $1903Dir -ItemType Directory | Out-Null
 }
 
 ## This file contains all of our scripts to run POST install. It's slim right now but the idea is to 
@@ -96,21 +96,21 @@ If (!(Test-Path $1903Dir)) {
 ## Check to see if the ISO file is already present
 $checkISO = Test-Path $1903ISO -PathType Leaf
 If ($checkISO) {
-  ## If the source ISO size is larger than the downloaded ISO size, nuke the download and start over.
-  ## This means the download was interrupted so if we continue the install will fail
-  If ($servFile -gt (Get-Item $1903ISO).Length) {
-    Remove-Item -Path $1903ISO -Force
-    ## We're setting $status to Download for a step further down so we know we still need the ISO downloaded and it's
-    ## not ready to install yet. You'll see $status set a few times below and it's all for the same reason.
-    $status = 'Download'
-    Write-Output "The existing installation files for the 1903 update were incomplete or corrupt. Deleted existing files and started a new download."
-  } Else {
-    Write-Output "Verified the installation package downloaded successfully!"
-    $status = 'Install'
-  }
+    ## If the source ISO size is larger than the downloaded ISO size, nuke the download and start over.
+    ## This means the download was interrupted so if we continue the install will fail
+    If ($servFile -gt (Get-Item $1903ISO).Length) {
+        Remove-Item -Path $1903ISO -Force
+        ## We're setting $status to Download for a step further down so we know we still need the ISO downloaded and it's
+        ## not ready to install yet. You'll see $status set a few times below and it's all for the same reason.
+        $status = 'Download'
+        Write-Output "The existing installation files for the 1903 update were incomplete or corrupt. Deleted existing files and started a new download."
+    } Else {
+        Write-Output "Verified the installation package downloaded successfully!"
+        $status = 'Install'
+    }
 } Else {
-  $status = 'Download'
-  Write-Output "The required files to install Windows 10 1903 are not present, downloading required files now. This download is 4.6GBs so may take awhile (depending on your connection speed)."
+    $status = 'Download'
+    Write-Output "The required files to install Windows 10 1903 are not present, downloading required files now. This download is 4.6GBs so may take awhile (depending on your connection speed)."
 }
 #endregion fileChecks
 
@@ -119,73 +119,73 @@ If ($checkISO) {
 ## Now we take that $status we set above and figure out if it needs to be downloaded. If $status
 ## does -eq Download then run thi section
 If ($status -eq 'Download') {
-  Try {
-    (New-Object System.Net.WebClient).DownloadFile($automate1903URL,$1903ISO)
-    ## Again check the downloaded file size vs the server file size
-    If ($servFile -gt (Get-Item $1903ISO).Length) {
-      Write-Warning "The downloaded size of $1903ISO does not match the server version, unable to install Windows 10 1903."
-    } Else {
-      Write-Output "Successfully downloaded the 1903 Windows 10 ISO!"
-      $status = 'Install'
+    Try {
+        (New-Object System.Net.WebClient).DownloadFile($automate1903URL,$1903ISO)
+        ## Again check the downloaded file size vs the server file size
+        If ($servFile -gt (Get-Item $1903ISO).Length) {
+        Write-Warning "The downloaded size of $1903ISO does not match the server version, unable to install Windows 10 1903."
+        } Else {
+        Write-Output "Successfully downloaded the 1903 Windows 10 ISO!"
+        $status = 'Install'
+        }
+    } Catch {
+        Write-Warning "Encountered a problem when trying to download the Windows 10 1903 ISO"
     }
-  } Catch {
-    Write-Warning "Encountered a problem when trying to download the Windows 10 1903 ISO"
-  }
 }
 
 ## If the portable ISO mount EXE doesn't exist then download it
 Try {
-  If (!(Test-Path $isoMountExe -PathType Leaf)) {
-    (New-Object System.Net.WebClient).DownloadFile($isoMountURL,$isoMountExe)
-  }
+    If (!(Test-Path $isoMountExe -PathType Leaf)) {
+        (New-Object System.Net.WebClient).DownloadFile($isoMountURL,$isoMountExe)
+    }
 } Catch {
-  Write-Warning "Encountered a problem when trying to download the ISO Mount EXE"
+    Write-Warning "Encountered a problem when trying to download the ISO Mount EXE"
 }
 
 Try {
-  ##Install
-  If ($status -eq 'Install') {
-    Write-Output "The Windows 10 Clean Install has now been started silently in the background. No action from you is required, but please note a reboot will be reqired during the installation prcoess. It is highly recommended you save all of your open files!"
-    $localFolder= (Get-Location).path
-    ## The portable ISO EXE is going to mount our image as a new drive and we need to figure out which drive
-    ## that is. So before we mount the image, grab all CURRENT drive letters
-    $curLetters = (Get-PSDrive).Name -match '^[a-z]$'
-    $osVer = (Get-WmiObject -class Win32_OperatingSystem).Caption
-    ## If the OS is Windows 10 or 8.1 we can mount the ISO native through Powershell
-    If ($osVer -like '*10*' -or $osVer -like '*8.1*') {
-      ## Mount the ISO with powershell
-      Mount-DiskImage $1903ISO
+    ##Install
+    If ($status -eq 'Install') {
+        Write-Output "The Windows 10 Clean Install has now been started silently in the background. No action from you is required, but please note a reboot will be reqired during the installation prcoess. It is highly recommended you save all of your open files!"
+        $localFolder= (Get-Location).path
+        ## The portable ISO EXE is going to mount our image as a new drive and we need to figure out which drive
+        ## that is. So before we mount the image, grab all CURRENT drive letters
+        $curLetters = (Get-PSDrive).Name -match '^[a-z]$'
+        $osVer = (Get-WmiObject -class Win32_OperatingSystem).Caption
+        ## If the OS is Windows 10 or 8.1 we can mount the ISO native through Powershell
+        If ($osVer -like '*10*' -or $osVer -like '*8.1*') {
+        ## Mount the ISO with powershell
+        Mount-DiskImage $1903ISO
+        } Else {
+            ## Install the portable ISO mount driver
+            cmd.exe /c "echo . | $isoMountExe /Install" | Out-Null
+            ## Mount the ISO
+            cmd.exe /c "echo . | $isoMountExe $1903ISO" | Out-Null
+        }
+        ## Have to sleep it here for a second because the ISO takes a second to mount and if we go too quickly
+        ## it will think no new drive letters exist
+        Start-Sleep 10
+        ## Now that the ISO is mounted we should have a new drive letter, so grab all drive letters again
+        $newLetters = (Get-PSDrive).Name -match '^[a-z]$'
+        ## Compare the drive letters from before/after mounting the ISO and figure out which one is new.
+        ## This will be our drive letter to work from
+        $mountedLetter = (Compare-Object -ReferenceObject $curLetters -DifferenceObject $newLetters).InputObject + ':'
+        ## Call setup.exe w/ all of our required install arguments
+        $process = Start-Process -FilePath "$mountedLetter\setup.exe" -ArgumentList "/Auto Clean /Quiet /Compat IgnoreWarning /ShowOOBE None /DynamicUpdate Enable /ResizeRecoveryPartition Enable /copylogs $windowslogs /PostOOBE $setupComplete" -PassThru
+        ## This is an attempt to show the install status w/ a progress bar for a GUI...it doesn't really work as intended
+        ## but it also doesn't break anything so it's just left here for now
+        For($i = 0; $i -le 100; $i = ($i + 1) % 100) {
+        Write-Progress -Activity "Installer" -PercentComplete $i -Status "Installing"
+        Start-Sleep -Milliseconds 100
+        If ($process.HasExited) {
+            Write-Progress -Activity "Installer" -Completed
+            Write-Output "Windows 10 Install process complete!"
+            Break
+        }
+        }
     } Else {
-      ## Install the portable ISO mount driver
-      cmd.exe /c "echo . | $isoMountExe /Install" | Out-Null
-      ## Mount the ISO
-      cmd.exe /c "echo . | $isoMountExe $1903ISO" | Out-Null
+        Write-Warning "Could not find a known status of the var Status. Output: $status"
     }
-    ## Have to sleep it here for a second because the ISO takes a second to mount and if we go too quickly
-    ## it will think no new drive letters exist
-    Start-Sleep 10
-    ## Now that the ISO is mounted we should have a new drive letter, so grab all drive letters again
-    $newLetters = (Get-PSDrive).Name -match '^[a-z]$'
-    ## Compare the drive letters from before/after mounting the ISO and figure out which one is new.
-    ## This will be our drive letter to work from
-    $mountedLetter = (Compare-Object -ReferenceObject $curLetters -DifferenceObject $newLetters).InputObject + ':'
-    ## Call setup.exe w/ all of our required install arguments
-    $process = Start-Process -FilePath "$mountedLetter\setup.exe" -ArgumentList "/Auto Clean /Quiet /Compat IgnoreWarning /ShowOOBE None /DynamicUpdate Enable /ResizeRecoveryPartition Enable /copylogs $windowslogs /PostOOBE $setupComplete" -PassThru
-    ## This is an attempt to show the install status w/ a progress bar for a GUI...it doesn't really work as intended
-    ## but it also doesn't break anything so it's just left here for now
-    For($i = 0; $i -le 100; $i = ($i + 1) % 100) {
-      Write-Progress -Activity "Installer" -PercentComplete $i -Status "Installing"
-      Start-Sleep -Milliseconds 100
-      If ($process.HasExited) {
-        Write-Progress -Activity "Installer" -Completed
-        Write-Output "Windows 10 Install process complete!"
-        Break
-      }
-    }
-  } Else {
-      Write-Warning "Could not find a known status of the var Status. Output: $status"
-  }
 } Catch {
-  Write-Warning "Setup ran into an issue while attempting to install the 1903 upgrade."
+    Write-Warning "Setup ran into an issue while attempting to install the 1903 upgrade."
 }
 #endregion download/install
