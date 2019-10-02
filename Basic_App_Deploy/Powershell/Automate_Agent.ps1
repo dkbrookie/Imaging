dc3a16a70a83edbfcd8f3e4dc7cc89904cbc6792
dkbrookie <matthew@dkbinnovative.net>
2019-10-02 15:13:02 -0500

    Update Automate_Agent.ps1

--- Basic_App_Deploy/Powershell/Automate_Agent.ps1
+++ Basic_App_Deploy/Powershell/Automate_Agent.ps1
@@ -2,4 +2,4 @@ If (!$locationID) {
     $locationID = 1
 }
 (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/dkbrookie/PowershellFunctions/master/Function.Install-MSI.ps1') | iex
-Install-MSI -AppName 'Automate' -Arguments "/qn LOCATIONID=$locationID" -FileDownloadLink 'https://support.dkbinnovative.com/Labtech/Deployment.aspx?Probe=1&installType=msi&MSILocations=1'
+Install-MSI -AppName 'Automate Agent' -Arguments "/qn LOCATIONID=$locationID" -FileDownloadLink 'https://support.dkbinnovative.com/Labtech/Deployment.aspx?Probe=1&installType=msi&MSILocations=1'
