a19a17c81076074eaf522899b3af1fd55a47c8eb
Seth Brasile <seth.brasile@gmail.com>
2022-05-05 13:57:23 -0500

    copypasta mistake

--- Windows_10_Staging/Powershell/Windows_10_Upgrade_V4/Windows_10_Upgrade_V4_Download.ps1
+++ Windows_10_Staging/Powershell/Windows_10_Upgrade_V4/Windows_10_Upgrade_V4_Download.ps1
@@ -513,11 +513,7 @@ If (Test-RegistryValue -Path 'HKLM:\SOFTWARE\LabTech\Service\Win10_20H2_Upgrade'
     $setupErr = Get-RegistryValue -Path 'HKLM:\SOFTWARE\LabTech\Service\Win10_20H2_Upgrade' -Name 'WindowsSetupError'
     $setupExitCode = Get-RegistryValue -Path 'HKLM:\SOFTWARE\LabTech\Service\Win10_20H2_Upgrade' -Name 'WindowsSetupExitCode'
     $outputLog = "!Error: Windows setup experienced an error upon last installation. This should be manually assessed and you should delete HKLM:\SOFTWARE\LabTech\Service\Win10_20H2_Upgrade\WindowsSetupError in order to make the script try again. The exit code was $setupExitCode and the error output was $setupErr" + $outputLog
-    Invoke-Output @{
-        outputLog                = $outputLog
-        installationAttemptCount = $installationAttemptCount
-    }
-    Return
+    Invoke-Output $outputLog
 }
 
 # Check that this upgrade hasn't already occurred. No need to download iso if installation has already occurred
